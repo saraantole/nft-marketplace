763b804cb43d35c3a957dbf1371dcd7a41b1425e
unknown <sara.antole@gmail.com>
2022-01-25 18:07:53 +0100

    fixing cards height

--- pages/index.js
+++ pages/index.js
@@ -70,8 +70,8 @@ export default function Home() {
                 <div key={i} className='border shadow rounded-xl overflow-hidden'>
                   <img src={nft.image} alt='nft' />
                   <div className='p-4'>
-                    <p style={{ height: '64px' }} className='text-2xl font-semibold'>{nft.name}</p>
-                    <div style={{ height: '70px', overflow: 'hidden' }}>
+                    <p className='text-2xl font-semibold'>{nft.name}</p>
+                    <div style={{ overflow: 'hidden' }}>
                       <p className='text-gray-400'>{nft.description}</p>
                     </div>
                   </div>
